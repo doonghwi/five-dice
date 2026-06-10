3bfae1e26f953d6d60722d1e52a569070b66038a
doonghwi <ehdgnlans@gmail.com>
2026-06-11 01:51:07 +0900

    사용량 핑 부착: dailyapp_stats.recordOpen (중앙 RTDB 자동 집계)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dailyapp_stats.dart';
 import 'theme.dart';
 import 'setup_screen.dart';
 
-void main() => runApp(const FiveDiceApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  DailyAppStats.recordOpen(
+    appId: 'five_dice',
+    name: '🎲 주사위 집계',
+    desc: '패스앤플레이 · 5주사위 모아 1~6 눈 개수 집계',
+    platforms: ['web', 'android', 'offline'],
+    webUrl: 'https://doonghwi.github.io/five-dice/',
+    repoUrl: 'https://github.com/doonghwi/five-dice',
+    day: 'Day 2',
+  );
+  runApp(const FiveDiceApp());
+}
 
 class FiveDiceApp extends StatelessWidget {
   const FiveDiceApp({super.key});
